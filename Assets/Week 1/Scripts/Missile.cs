4c654abf8ae9fd2c7daea625f959dd39df64f85d
SHERNET\barryki <kit.barry@sheridancollege.ca>
2024-01-17 08:37:36 -0500

    Task 3 & 4 ready for Week 2

---
+++ Assets/Week 1/Scripts/Missile.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Missile : MonoBehaviour
+{
+    public float speed = 10f;
+    void Update()
+    {
+        transform.Translate(speed * Time.deltaTime, 0, 0);
+    }
+}
